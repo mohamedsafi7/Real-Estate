91e2bbc20330ce0571d5d527ea9acee80307d6c9
aminesabri <aminesabri@gmail.com>
2024-04-20 19:17:38 +0100

    filters

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,13 +3,21 @@
 namespace App\Http\Controllers;
 
 use App\Models\Category;
+use App\Models\Proprety;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
     public function get()
     {
+        $listings = Proprety::with(['listingType', 'category', 'images'])->get();
+        $properties = Proprety::with('images')->get();
         $categories = Category::all();
-        return view('welcome', compact('categories'));
+        return view('welcome', compact('categories','properties','listings'));
     }
+
+
+
+    
+
 }
